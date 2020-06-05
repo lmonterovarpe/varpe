65bba9f4cbf3a971a24c260fae49d7388bc42488
Raul Carbonell <60281331+RaulCarbonell@users.noreply.github.com>
2020-06-05 09:41:06 +0200

    [IMP] Product Only Code. Mostrar en sale.order.line, el nombre del producto en el campo name, al seleccionar el codigo del producto

--- Product_OnlyCode/models/product_only_code.py
+++ Product_OnlyCode/models/product_only_code.py
@@ -83,3 +83,15 @@ class ProductOnlyCode(models.Model):
                           }
                 result.append(_name_get(mydict))
         return result
+
+
+    def get_product_multiline_description_sale(self):
+        """ Compute a multiline description of this product, in the context of sales
+                (do not use for purchases or other display reasons that don't intend to use "description_sale").
+            It will often be used as the default description of a sale order line referencing this product.
+        """
+        name = self.name
+        if self.description_sale:
+            name += '\n' + self.description_sale
+
+        return name
