edae284a5921ef208551a329fc203beb3c0f6f42
Raul Carbonell <60281331+RaulCarbonell@users.noreply.github.com>
2020-06-05 09:48:12 +0200

    [ADD] Product Only Code.
    Mostrar sólo el código del producto en el momento de seleccionar un producto.
    Y en sale.order.line, añadir sólo la descripción del producto en el campo descripción

---
+++ Product_OnlyCode/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Raul Carbonell - raul.carbonell@processcontrol.es
+# License AGPL-3.0 or later
+{
+    "name": "Product - Show Only Code",
+    "summary":
+         "Mostrar sólo el código del producto en el momento de seleccionar un producto",
+    "version": "13.0.1.0.0",
+    "category": "undefined",
+    "website": "",
+    "author": "Raul Carbonell",
+    "license": "AGPL-3",
+    "data": [
+    ],
+    "depends": [
+        "base",
+    ],
+    "application": False,
+    "installable": True,
+}
